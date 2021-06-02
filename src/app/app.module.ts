39a8ca3ffcdb40403bc5c741c1c107d63e42ebae
Wanessa Sales <wanessacomputer@gmail.com>
2021-06-01 21:05:20 -0300

    criacao componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
+import { ComentarioComponent } from './componente/comentario/comentario.component';
+import { UsuarioComponent } from './componente/usuario/usuario.component';
+import { PostagemComponent } from './componente/postagem/postagem.component';
+import { CompartilhamentoComponent } from './componente/compartilhamento/compartilhamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent,
+    ComentarioComponent,
+    UsuarioComponent,
+    PostagemComponent,
+    CompartilhamentoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componente/compartilhamento/compartilhamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compartilhamento',
+  templateUrl: './compartilhamento.component.html',
+  styleUrls: ['./compartilhamento.component.css']
+})
+export class CompartilhamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
